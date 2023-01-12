1f70ee9df44da3e4aac17ed01c41417ffc56031b
hanbings <3219065882@qq.com>
2023-01-12 14:55:54 +0800

    ✨ Feat: #Publish Add Discord profile entity.

---
+++ fluocean-discord/src/main/java/io/hanbings/fluocean/discord/DiscordProfile.java
@@ -0,0 +1,53 @@
+package io.hanbings.fluocean.discord;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+
+public record DiscordProfile(
+        @JsonProperty("premium_type")
+        @SerializedName("premium_type")
+        int premiumType,
+        @JsonProperty("accent_color")
+        @SerializedName("accent_color")
+        int accentColor,
+        @JsonProperty("verified")
+        @SerializedName("verified")
+        boolean verified,
+        @JsonProperty("flags")
+        @SerializedName("flags")
+        int flags,
+        @JsonProperty("banner")
+        @SerializedName("banner")
+        String banner,
+        @JsonProperty("id")
+        @SerializedName("id")
+        String id,
+        @JsonProperty("avatar")
+        @SerializedName("avatar")
+        String avatar,
+        @JsonProperty("public_flags")
+        @SerializedName("public_flags")
+        int publicFlags,
+        @JsonProperty("email")
+        @SerializedName("email")
+        String email,
+        @JsonProperty("username")
+        @SerializedName("username")
+        String username,
+        @JsonProperty("discriminator")
+        @SerializedName("discriminator")
+        String discriminator
+) {
+    record Wrong(
+            @JsonProperty("code")
+            @SerializedName("code")
+            int code,
+            @JsonProperty("errors")
+            @SerializedName("errors")
+            Object errors,
+            @JsonProperty("message")
+            @SerializedName("message")
+            String message
+    ) {
+    }
+}
