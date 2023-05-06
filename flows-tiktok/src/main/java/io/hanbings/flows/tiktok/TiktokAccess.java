deeab1354b15922dbdde07da3b43bdfce8ed11f3
hanbings <hanbings@hanbings.io>
2023-05-07 02:20:30 +0800

    ✨ Feat: Add tiktok package.

---
+++ flows-tiktok/src/main/java/io/hanbings/flows/tiktok/TiktokAccess.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 Flows
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.hanbings.flows.tiktok;
+
+import io.hanbings.flows.common.interfaces.Access;
+
+public record TiktokAccess(
+        String accessToken
+) implements Access {
+    public record Wrong(
+            String error,
+            String errorDescription
+    ) implements Access.Wrong {
+    }
+}
